7869e0bd5c3cd1bc0b496798e9400ecc2755e65d
nehashri <nehashrl@thoughtworks.com>
2017-09-12 15:06:03 +0530

    Looking for correct vsix files in release scripts

--- github-release.sh
+++ github-release.sh
@@ -7,13 +7,13 @@ fi
 
 cd artifacts
 
-version=$(ls Gauge-*.vsix | sed "s/^Gauge-\([^;]*\).vsix/\1/")
+version=$(ls artifact/gauge-*.vsix | sed "s/^artifact\/gauge-\([^;]*\).vsix/\1/")
 repoName="gauge-vscode"
 releaseName="Gauge VSCode Extension $version"
-artifact="Gauge-$version.vsix"
+artifact="gauge-$version.vsix"
 
 releaseDescription=$(ruby -e "$(curl -sSfL https://github.com/getgauge/gauge/raw/master/build/create_release_text.rb)" $repoName)
 
 $GOPATH/bin/github-release release -u getgauge -r "$repoName" --draft -t "v$version" -d "$releaseDescription" -n "$releaseName"
 
-$GOPATH/bin/github-release -v upload -u getgauge -r "$repoName" -t "v$version" -n "$artifact" -f "$artifact"
+$GOPATH/bin/github-release -v upload -u getgauge -r "$repoName" -t "v$version" -n "$artifact" -f "artifact/$artifact"
--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-version=$(ls gauge-*.vsix | sed "s/^gauge-\([^;]*\).vsix/\1/")
+version=$(ls artifact/gauge-*.vsix | sed "s/^artifact\/gauge-\([^;]*\).vsix/\1/")
 npm install
 npm run publish -- --packagePath artifact/gauge-$version.vsix -p $VS_PAT
