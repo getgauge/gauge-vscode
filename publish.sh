115af3e5e8da2a02fd99aa1c8e86d6f601e0ca6e
nehashri <nehashrl@thoughtworks.com>
2017-09-12 14:44:35 +0530

    Changing publish script

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
-version=$(ls Gauge-*.vsix | sed "s/^Gauge-\([^;]*\).vsix/\1/")
-npm run publish -- --packagePath artifact/Gauge-$version.vsix -p $VS_PAT
+version=$(ls gauge-*.vsix | sed "s/^gauge-\([^;]*\).vsix/\1/")
+npm run publish -- --packagePath artifact/gauge-$version.vsix -p $VS_PAT
