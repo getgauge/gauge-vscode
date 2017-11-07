af9164186fe7de7e5dbab4372323f1a2ead11280
nehashri <nehashrl@thoughtworks.com>
2017-11-07 14:37:08 +0530

    increasing timeout for tests

--- test/execution/execution.test.ts
+++ test/execution/execution.test.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { execute } from '../../src/execution/gaugeExecution'
 
 suite('Gauge Execution Tests', () => {
-
+	this.timeout(10000);
 	test('should execute given specification', (done) => {
 		let spec = path.join(__dirname, '..', '..', '..', 'test', 'testdata', 'sampleProject', 'specs', 'example.spec');
 		execute(spec,false).then((status) => {
