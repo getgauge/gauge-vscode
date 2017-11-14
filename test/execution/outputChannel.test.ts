0de724bcc1c5acc1b8c2d9de45473913f23d1c72
sriv <srikanth.ddit@gmail.com>
2017-11-14 14:53:32 +0530

    fix tests, have proper setup

--- test/execution/outputChannel.test.ts
+++ test/execution/outputChannel.test.ts
@@ -7,9 +7,7 @@ suite('Output Channel', () => {
 	class MockVSOutputChannel implements vscode.OutputChannel{
 		name: string;
 		text: string;
-		append(value: string): void {
-			this.text += value;
-		}
+		append(value: string): void {}
 		appendLine(value: string): void {
 			this.text = value;
 		}
@@ -30,18 +28,18 @@ suite('Output Channel', () => {
 	test('should convert specification path from relative to absolute', (done) => {
 		var chan = new MockVSOutputChannel();
 		var outputChannel = new OutputChannel(chan, "");
-		outputChannel.appendOutBuf("      Specification: specs" + path.sep + "example.spec:19");
+		outputChannel.appendOutBuf("      Specification: " + path.join("specs", "example.spec:19") + "\n");
 
-		assert.equal(chan.read(), "      Specification: "+ vscode.workspace.rootPath + "specs" + path.sep + "example.spec:19");
+		assert.equal(chan.read(), "      Specification: "+ path.join(vscode.workspace.rootPath, "specs", "example.spec:19"));
 		done();
 	});
 
 	test('should convert implementation path from relative to absolute', (done) => {
 		var chan = new MockVSOutputChannel();
 		var outputChannel = new OutputChannel(chan, "");
-		outputChannel.appendOutBuf("      at Object.<anonymous> (tests" + path.sep + "step_implementation.js:24:10)");
+		outputChannel.appendOutBuf("      at Object.<anonymous> (" + path.join("tests", "step_implementation.js:24:10") +")\n");
 
-		assert.equal(chan.read(), "      at Object.<anonymous> (tests"+ vscode.workspace.rootPath + "specs" + path.sep + "step_implementation.js:24:10)");
+		assert.equal(chan.read(), "      at Object.<anonymous> ("+ path.join(vscode.workspace.rootPath,"tests", "step_implementation.js:24:10)"));
 		done();
 	});
 });
