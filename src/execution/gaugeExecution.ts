b186b1b2d080a007e5f483415af84df90e9f71bf
Apoorva G A <apoorvag@thoughtworks.com>
2018-02-08 14:36:04 +0530

    Stopping the debugger when the user stops execution #124

--- src/execution/gaugeExecution.ts
+++ src/execution/gaugeExecution.ts
@@ -68,8 +68,11 @@ function setDebugConf(config: any): Thenable<any> {
 }
 
 export function cancel() {
-	if (childProcess && !childProcess.killed)
+	if (childProcess && !childProcess.killed){
+		let activeDebugSession = debug.activeDebugSession;
+		activeDebugSession.customRequest("disconnect");
 		childProcess.kill();
+	}
 }
 
 export function onBeforeExecute(hook: Function) {
