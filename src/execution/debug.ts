9b05f7b5d241cced2b3c608eafa63908d720c926
Saakallya Biswas <riju91@gmail.com>
2018-03-15 12:15:28 +0530

    Attaching debugger after receiving event through stdout, #getgauge/gauge-python#36

--- src/execution/debug.ts
+++ src/execution/debug.ts
@@ -3,65 +3,82 @@
 import { window, workspace, debug } from 'vscode';
 import getPort = require('get-port');
 
-export let clientLanguageMap: Map<string, string> = new Map();
+const GAUGE_DEBUGGER_NAME = "Gauge Debugger";
+const REQUEST_TYPE = "attach";
+const DEBUG_PORT = 'debugPort';
 
-export function setDebugConf(config: any, debugPort: string): Thenable<any> {
-    let env = Object.create(process.env);
-    if (config.debug) {
-        env.DEBUGGING = true;
-        return getPort({ port: debugPort }).then((port) => {
-            env.DEBUG_PORT = port;
-            GaugeDebugger.attach(clientLanguageMap.get(config.projectRoot), port, config.projectRoot);
-            return env;
-        });
-    } else {
-        return Promise.resolve(env);
-    }
-}
+export class GaugeDebugger {
 
-class GaugeDebugger {
-    python(port: number, projectRoot: string): void {
-        let debugConfig = {
-            type: "python",
-            name: "Gauge Debugger",
-            request: "attach",
-            port: port,
-            localRoot: projectRoot
-        };
-        setTimeout(() => {
-            debug.startDebugging(workspace.getWorkspaceFolder(window.activeTextEditor.document.uri),
-                debugConfig);
-        }, 1500);
+    private debugPort: number;
+    private languageId: string;
+    private projectRoot: string;
+    private debug: boolean;
+
+    constructor(clientLanguageMap: Map<string, string>, config: any) {
+        this.languageId = clientLanguageMap.get(config.projectRoot);
+        this.projectRoot = config.projectRoot;
+        this.debug = config.debug;
     }
 
-    javascript(port: number, projectRoot: string) {
-        let debugConfig = {
-            type: "node",
-            name: "Gauge Debugger",
-            request: "attach",
-            port: port,
-            protocol: "inspector"
-        };
+    private setDebuggerConf(): any {
+        switch (this.languageId) {
+            case "python": {
+                return {
+                    type: "python",
+                    name: GAUGE_DEBUGGER_NAME,
+                    request: REQUEST_TYPE,
+                    port: this.debugPort,
+                    localRoot: this.projectRoot
+                };
+            }
 
+            case "javascript": {
+                return {
+                    type: "node",
+                    name: GAUGE_DEBUGGER_NAME,
+                    request: REQUEST_TYPE,
+                    port: this.debugPort,
+                    protocol: "inspector"
+                };
+            }
+
+            case "ruby": {
+                return {
+                    name: GAUGE_DEBUGGER_NAME,
+                    type: "Ruby",
+                    request: REQUEST_TYPE,
+                    cwd: this.projectRoot,
+                    remoteWorkspaceRoot: this.projectRoot,
+                    remoteHost: "127.0.0.1",
+                    remotePort: this.debugPort
+                };
+            }
+        }
+    }
+
+    public addDebugEnv(): Thenable<any> {
+        let env = Object.create(process.env);
+        if (this.debug) {
+            env.DEBUGGING = true;
+            return getPort({ port: DEBUG_PORT }).then((port) => {
+                env.DEBUG_PORT = port;
+                this.debugPort = port;
+                return env;
+            });
+        } else {
+            return Promise.resolve(env);
+        }
+    }
+
+    public startDebugger(): void {
         debug.startDebugging(workspace.getWorkspaceFolder(window.activeTextEditor.document.uri),
-            debugConfig);
+        this.setDebuggerConf());
     }
 
-    ruby(port: number, projectRoot: string) {
-        let debugConfig = {
-            name: "Gauge Debugger",
-            type: "Ruby",
-            request: "attach",
-            cwd: projectRoot,
-            remoteWorkspaceRoot: projectRoot,
-            remoteHost: "127.0.0.1",
-            remotePort: port
-        };
-        debug.startDebugging(workspace.getWorkspaceFolder(window.activeTextEditor.document.uri),
-            debugConfig);
+    public stopDebugger(): void {
+        if (debug.activeDebugSession) {
+            debug.activeDebugSession.customRequest("disconnect");
+        }
     }
 
-    static attach(language: string, port: number, projectPath: string) {
-        this.prototype[language](port, projectPath);
-    }
 }
