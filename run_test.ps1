b3ccb47057a239f5de87546842f6b55aa9302d37
sriv <srikanth.ddit@gmail.com>
2018-04-20 07:04:46 +0530

    docker run win build on appveyor

---
+++ run_test.ps1
@@ -0,0 +1,18 @@
+$env:GAUGE_PREFIX=C:\GAUGE
+
+mkdir C:\GAUGE
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/getgauge/infrastructure/master/nightly_scripts/install_latest_gauge_nightly.ps1" -OutFile install_latest_gauge_nightly.ps1
+.\install_latest_gauge_nightly.ps1
+
+$env:Path = "$env:GAUGE_PREFIX;" + [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+gauge config gauge_repository_url https://raw.githubusercontent.com/getgauge/gauge-nightly-repository/master/
+
+gauge.exe install js
+gauge.exe install html-report
+gauge.exe version
+
+npm run build
+
+npm test
