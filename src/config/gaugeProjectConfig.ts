e440b4a808b106c660c019d4a70d6d8fd599878a
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-01-11 15:00:00 +0530

    Use installed java version to define .classpath file. #323

--- src/config/gaugeProjectConfig.ts
+++ src/config/gaugeProjectConfig.ts
@@ -2,61 +2,66 @@ import * as path from 'path';
 import * as glob from 'glob';
 import { writeFileSync } from 'fs';
 import { homedir } from 'os';
+import { exec } from 'child_process';
 import * as xmlbuilder from 'xmlbuilder';
 import { isProjectLanguage, isMavenProject } from "../util";
 import { getGaugeVersionInfo } from '../gaugeVersion';
 const javaPluginPath = path.join(homedir(), '.gauge', 'plugins', 'java');
-
+const DEFAULT_JAVA_VERSION = '11';
 export class GaugeJavaProjectConfig {
     projectRoot: any;
-    private readonly defaultCLassPathEntries = [
-        {
-            kind: 'con',
-            path: `org.eclipse.jdt.launching.JRE_CONTAINER/` +
-            `org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.8`
-        },
-        {
-            kind: 'src',
-            path: 'src/test/java'
-        },
-        {
-            kind: 'output',
-            path: 'gauge_bin'
-        }
-    ];
-
     constructor(projectRoot) {
         this.projectRoot = projectRoot;
     }
+
+    private defaultCLassPath(javaVersion: string) {
+        return [
+            {
+                kind: 'con',
+                path: `org.eclipse.jdt.launching.JRE_CONTAINER/` +
+                    `org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-${javaVersion}`
+            },
+            {
+                kind: 'src',
+                path: 'src/test/java'
+            },
+            {
+                kind: 'output',
+                path: 'gauge_bin'
+            }
+        ];
+    }
+
     generate() {
         if (isProjectLanguage(this.projectRoot, 'java') && !isMavenProject(this.projectRoot)) {
-            this.createDotClassPathFile(path.join(this.projectRoot, '.classpath'));
+            exec('java -version', (err, __, out) => {
+                const dotCPFilePath = path.join(this.projectRoot, '.classpath');
+                let javaVersion = out.replace(/.*?(\d+\.\d+\.\d+).*(\s+.*)+/, '$1');
+                if ( err !== null || !out ) {
+                    return this.createDotClassPathFile(dotCPFilePath, DEFAULT_JAVA_VERSION);
+                }
+                if (javaVersion.match(/^\d\./)) {
+                    this.createDotClassPathFile(dotCPFilePath, javaVersion.replace(/(\.\d+(\w+)?$)/, ''));
+                } else {
+                    this.createDotClassPathFile(dotCPFilePath, javaVersion.replace(/\.\d+/g, ''));
+                }
+            });
             this.createDotProjectFile(path.join(this.projectRoot, '.project'));
         }
     }
-    private createDotClassPathFile(cpFilePath: string) {
-        let versionInfo = getGaugeVersionInfo();
-        let javaPluginInfo = versionInfo.plugins.find((plugin) => plugin.hasName('java'));
+    private createDotClassPathFile(cpFilePath: string, javaVersion: string) {
+        let { plugins } = getGaugeVersionInfo();
+        let javaPluginInfo = plugins.find((plugin) => plugin.hasName('java'));
         let jars = glob.sync(`${javaPluginInfo.version}/libs/*.jar`, { cwd: javaPluginPath })
-            .filter( (jar) =>  jar.match(/gauge|assertj-core/));
-
-        let defaultCLassPathEntries = this.defaultCLassPathEntries
-            .map((entry) => this.cpEntry(entry.kind, entry.path));
+            .filter((jar) => jar.match(/gauge|assertj-core/));
         let classPathForJars = jars
             .map((jar) => this.cpEntry('lib', path.join(javaPluginPath, jar)));
-
         let classPathObj = {
-            classpath: { classpathentry: [...defaultCLassPathEntries, ...classPathForJars] }
+            classpath: { classpathentry: [...this.getDefaultCPEntries(javaVersion), ...classPathForJars] }
         };
-        writeFileSync(cpFilePath, xmlbuilder.create(classPathObj, { encoding: 'UTF-8' }).end({ pretty: true }));
+        this.configFile(cpFilePath, classPathObj);
     }
 
-    private cpEntry(kind, path) {
-        return {
-            '@kind': kind,
-            '@path': path
-        };
-    }
     private createDotProjectFile(projectFilePath) {
         const projectObj = {
             projectDescription: {
@@ -74,8 +79,24 @@ export class GaugeJavaProjectConfig {
                 }
             }
         };
-        const xml = xmlbuilder.create(projectObj, { encoding: 'UTF-8' });
+        this.configFile(projectFilePath, projectObj);
+    }
+
+    private configFile(filePath, contentObj) {
+        const xml = xmlbuilder.create(contentObj, { encoding: 'UTF-8' });
         let content = xml.end({ pretty: true });
-        writeFileSync(projectFilePath, content);
+        writeFileSync(filePath, content);
+    }
+
+    private getDefaultCPEntries(javaVersion) {
+        return this.defaultCLassPath(javaVersion)
+        .map((entry) => this.cpEntry(entry.kind, entry.path));
+    }
+
+    private cpEntry(kind, path) {
+        return {
+            '@kind': kind,
+            '@path': path
+        };
     }
 }
