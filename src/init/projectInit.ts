39479c540b3a2895176761e14cecc14e4aaf062d
Subham Singh <s.chauhan5505@gmail.com>
2019-09-24 13:48:34 +0530

    Add template for java gradle, #401

--- src/init/projectInit.ts
+++ src/init/projectInit.ts
@@ -26,6 +26,7 @@ export class ProjectInitializer extends Disposable {
         { name: 'java', desc: "template for gauge-java projects", },
         { name: 'java_maven', desc: "template for gauge-java projects with maven as build tool.", },
         { name: 'java_maven_selenium', desc: "template for gauge-java selenium projects with maven as build tool.", },
+        { name: 'java_gradle', desc: "template for gauge-gradle projects with gradle as build tool.", },
         { name: 'ts', desc: "template for gauge-typescript projects.", },
         { name: 'dotnet', desc: "template for gauge-dotnet projects.", }
     ];
