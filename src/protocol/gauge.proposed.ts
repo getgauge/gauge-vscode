6077d4fe21b7216e978c185e1b138e59810fcec2
NivedhaSenthil <nivedhasenthil@gmail.com>
2020-12-16 16:58:29 +0530

    update dependencies
    Signed-off-by: NivedhaSenthil <nivedhasenthil@gmail.com>

--- src/protocol/gauge.proposed.ts
+++ src/protocol/gauge.proposed.ts
@@ -12,7 +12,7 @@ export interface GaugeClientCapabilities {
  * The `workspace/saveFiles` notification is sent from the server to the client to save all open files in the workspace.
  */
 export namespace SaveFilesRequest {
-    export const type = new RequestType0<any, void, void>('workspace/saveFiles');
+    export const type = new RequestType0<any, void>('workspace/saveFiles');
     export type HandlerSignature = RequestHandler0<null, void>;
     export type MiddlewareSignature = (token: CancellationToken, next: HandlerSignature) => HandlerResult<null, void>;
 }
