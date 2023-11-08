477d9e5d50e2755ad2505d581ba95ea601e42b0e
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2023-11-08 10:11:23 +0100

    test stated version compatibility
    this commit adds a new testing mode whereby the extension
    is tested against the stated minimum vscode compatibility
    
    this test fails in this commit due to a minimum @types/vscode
    requirement of the lsp packages that breaks during runtime
    
    Signed-off-by: Lukas Bockstaller <lukas.bockstaller@posteo.de>

--- test/runTest.ts
+++ test/runTest.ts
@@ -1,18 +1,25 @@
 import { runTests } from '@vscode/test-electron';
 import * as path from 'path';
+import { engines } from "../package.json";
 
-async function go() {
+async function go(version?: string) {
     try {
         const extensionDevelopmentPath = path.resolve(__dirname, '../../');
         const extensionTestsPath = path.resolve(__dirname, './');
         const testWorkspace = path.resolve(__dirname, '../../test/testdata/sampleProject');
 
+        if (version===undefined){
+            // run tests with current stable vscode version
+            version = "stable";
+        }
+        console.log(`Running tests with version: ${version}`)
+
         await runTests({
             extensionDevelopmentPath,
             extensionTestsPath,
-            launchArgs: [testWorkspace, '--disable-extensions']
+            launchArgs: [testWorkspace, '--disable-extensions'],
+            version: version
         });
-
 
     } catch (err) {
         console.error('---Failed to run tests---');
@@ -21,4 +28,32 @@ async function go() {
     }
 }
 
-go()
+console.log(process.argv);
+
+if (process.argv[2] && process.argv[2] === '--compatibility') {
+    // Running the tests with the minimum vscode version that this plugin supports.
+    // The version is directly taken from this plugins package version
+    //
+    // This rerun is here to catch incompatibilities of @types/vscode with the lsp
+    // packages:
+    // - vscode-jsonrpc
+    // - vscode-languageclient
+    // - vscode-languageclient-protocol
+
+    // You can upgrade vscode-jsonrpc, vscode-languageclient and vscode-languageclient-protocol
+    // but must do so in one step, as these are released in tandem
+    // see https://github.com/Microsoft/vscode-languageserver-node
+
+    // Upgrading them might require a @types/vscode version bump (these version bumps are
+    // not mentioned in their release notes).
+    // Bumping @types/vscode will require bumping the minimum supported vscode version in
+    // package.json (engines["vscode"]) as well.
+    // The value of engines["vscode"] cannot be a pinned version "~1.67.0" but must use the
+    // minimum version notation "^1.67.0" indicating that this is the minimum supported
+    // vscode version
+    const version = engines["vscode"].replace("^", "");
+    go(version)
+} else {
+    go()
+}
+
