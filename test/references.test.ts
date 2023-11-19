19b246014335bdcf81083927a91dcaa862b1c0fb
Chad Wilson <chadw@thoughtworks.com>
2023-11-19 23:41:13 +1300

    Replace clipboardy with use of native vscode API (#902)
    * Replace clipboardy with use of native vscode API
    
    Signed-off-by: Chad Wilson <chadw@thoughtworks.com>
    
    * Increase test setup timeout for references tests
    
    Signed-off-by: Chad Wilson <chadw@thoughtworks.com>
    
    ---------
    
    Signed-off-by: Chad Wilson <chadw@thoughtworks.com>

--- test/references.test.ts
+++ test/references.test.ts
@@ -4,7 +4,9 @@ import * as path from 'path';
 
 let testDataPath = path.join(__dirname, '..', '..', 'test', 'testdata', 'sampleProject');
 
-suite('Gauge References Tests', () => {
+suite('Gauge References Tests', function () {
+    this.timeout(10000);
+
     setup(async () => {
         await commands.executeCommand('workbench.action.closeAllEditors');
         let implFile = Uri.file(path.join(testDataPath, 'tests', 'step_implementation.js'));
@@ -16,12 +18,12 @@ suite('Gauge References Tests', () => {
         await commands.executeCommand("cursorMove", { to: 'down', by: 'line', value: 18 });
         let value = commands.executeCommand('gauge.showReferences.atCursor');
         assert.ok(value);
-    }).timeout(10000);
+    });
 
     test('should not show any reference if cursor is not in step context', async () => {
         await commands.executeCommand("workbench.action.focusFirstEditorGroup");
         await commands.executeCommand("cursorMove", { to: 'down', by: 'line', value: 20 });
         let value = commands.executeCommand('gauge.showReferences.atCursor');
         assert.notEqual(value, true);
-    }).timeout(10000);
+    });
 });
